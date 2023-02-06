0e33b13a66ffbf93987bcf605a8b2fd78642f1ea
Simon Berger <simon.c.berger@pm.me>
2023-02-06 13:20:24 +0100

    Optimized script

--- tests/run_simple_tests.sh
+++ tests/run_simple_tests.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Get script directory
 if [ -L $0 ] ; then
@@ -8,28 +8,32 @@ else
 fi ;
 
 # Clean results
-rm -rf $SCRIPT_DIR/simple_tests_results
-mkdir $SCRIPT_DIR/simple_tests_results
+rm -rf ${SCRIPT_DIR}/simple_tests_results
+mkdir ${SCRIPT_DIR}/simple_tests_results
 
 # Iterate over all test files
-for TEST_FILE in $SCRIPT_DIR/simple_tests_files/*
+for TEST_FILE in ${SCRIPT_DIR}/simple_tests_files/*
 do
+    # Get base name of test
+    TEST_NAME=$(basename "${TEST_FILE}" .c)
+
+    # Create directory for test results
+    TEST_DIR=${SCRIPT_DIR}/simple_tests_results/${TEST_NAME}
+    mkdir ${TEST_DIR}
+
     # Create LLVM IR
-    clang -S -c -Xclang -O0 -emit-llvm $TEST_FILE -o $SCRIPT_DIR/simple_tests_results/$(basename "$TEST_FILE" .c).ll
+    clang -S -c -Xclang -O0 -emit-llvm ${TEST_FILE} -o ${TEST_DIR}/${TEST_NAME}.ll
     
     # Compile to binary
-    clang -O0 $TEST_FILE -o $SCRIPT_DIR/simple_tests_results/$(basename "$TEST_FILE" .c)
+    clang -O0 ${TEST_FILE} -o ${TEST_DIR}/${TEST_NAME}.bin
 
     # Create graphs and data from LLVM IR using SVF
-    wpa -nander -svfg -dump-vfg -dump-callgraph -dump-pag -write-ander $SCRIPT_DIR/simple_tests_results/$(basename "$TEST_FILE" .c).ander -write-svfg $SCRIPT_DIR/simple_tests_results/$(basename "$TEST_FILE" .c).svfg $SCRIPT_DIR/simple_tests_results/$(basename "$TEST_FILE" .c).ll
-    mv svfg_final.dot $SCRIPT_DIR/simple_tests_results/$(basename "$TEST_FILE" .c)_svfg.dot
-    mv callgraph_final.dot $SCRIPT_DIR/simple_tests_results/$(basename "$TEST_FILE" .c)_callgraph.dot
-    mv svfir_initial.dot $SCRIPT_DIR/simple_tests_results/$(basename "$TEST_FILE" .c)_svfir.dot
+    wpa -nander -svfg -show-hidden-nodes -svfg-with-ind-call -rp-td-edge -dump-vfg -dump-callgraph -dump-pag -write-ander ${TEST_DIR}/${TEST_NAME}_wpa.ander -write-svfg ${TEST_DIR}/${TEST_NAME}_wpa.svfg ${TEST_DIR}/${TEST_NAME}.ll
+    mv svfg_final.dot ${TEST_DIR}/${TEST_NAME}_wpa_svfg.dot
+    mv callgraph_final.dot ${TEST_DIR}/${TEST_NAME}_wpa_callgraph.dot
+    mv svfir_initial.dot ${TEST_DIR}/${TEST_NAME}_wpa_svfir.dot
     rm callgraph_initial.dot
-    wpa -type -dump-icfg $SCRIPT_DIR/simple_tests_results/$(basename "$TEST_FILE" .c).ll
-    mv icfg_initial.dot $SCRIPT_DIR/simple_tests_results/$(basename "$TEST_FILE" .c)_icfg.dot
-    mv vfg_initial.dot $SCRIPT_DIR/simple_tests_results/$(basename "$TEST_FILE" .c)_vfg.dot
-    
-    saber -nander -dump-free -dump-vfg $SCRIPT_DIR/simple_tests_results/$(basename "$TEST_FILE" .c).ll
-    mv svfg_final.dot $SCRIPT_DIR/simple_tests_results/$(basename "$TEST_FILE" .c)_saber_svfg.dot
+    wpa -type -dump-icfg ${TEST_DIR}/${TEST_NAME}.ll
+    mv icfg_initial.dot ${TEST_DIR}/${TEST_NAME}_wpa_icfg.dot
+    mv vfg_initial.dot ${TEST_DIR}/${TEST_NAME}_wpa_vfg.dot
 done
